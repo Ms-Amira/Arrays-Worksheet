a4351f28b980e0c3381ce452da0b5875fc229609
Mimi <trish21215@yahoo.com>
2025-04-09 23:24:13 -0400

    set up structure of eexercise

---
+++ script.js
@@ -0,0 +1,13 @@
+// Part 1: Understanding Basics of Arrays
+
+// Part 2: Adding and Removing Elements
+
+// Part 2: Adding and Removing Elements
+
+// Part 4: Hands-On Practice
+
+// Part 5 : Conditional Statements with arrays
+
+// Part 6 : Print Even Indexed Elements
+
+// Part 7 : Push Challenge: Nested Loops
