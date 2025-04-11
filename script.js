d3868fe6245ed4c793c0baf1cda67eb6398e2645
Mimi <trish21215@yahoo.com>
2025-04-11 13:12:52 -0400

    finished most of part 3

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 // Part 1: Understanding Basics of Arrays
+let fruits = ['apple', 'banana', 'cherry'];
+
+console.log(fruits[1]);
+console.log(fruits.length);
 
 // Part 2: Adding and Removing Elements
+fruits.push('orange');
+console.log(fruits);
 
-// Part 2: Adding and Removing Elements
+fruits.pop();
+console.log(fruits);
+
+fruits.push('guava', 'lychee');
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits.length);
+
+//Part 3: Looping Through Arrays
+for (let f in fruits) {
+    console.log(fruits[f]);
+};
+
+for (let f = 0; f < fruits.length; f++) {
+    console.log(fruits[f].charAt(0).toUpperCase() + fruits[f].slice(1).toLowerCase())
+};
+
+
+
 
 // Part 4: Hands-On Practice
 
